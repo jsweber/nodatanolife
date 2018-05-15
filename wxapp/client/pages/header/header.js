0080edc1dccb626592f95ba764eee28befabfdaf
duxq <dxq.duxq@lieluobo.com>
2018-05-15 18:39:00 +0800

    test2

---
+++ wxapp/client/pages/header/header.js
@@ -0,0 +1,15 @@
+// Page({
+//     data: {
+//         array: [1,2,3,4,5]
+//     }
+// })
+
+Component({
+    properties: {
+        array: {
+            type: Array,
+            value: [1,2,3,4,5]
+        }
+    },
+    data: {}
+})
