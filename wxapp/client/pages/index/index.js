b919fcd7d75c6904efb20fc9f31c7f6c4067b792
duxq <dxq.duxq@lieluobo.com>
2018-06-12 17:21:33 +0800

    finish index style

--- wxapp/client/pages/index/index.js
+++ wxapp/client/pages/index/index.js
@@ -4,6 +4,19 @@ var util = require('../../utils/util.js')
 
 Page({
     data: {
-        jobArr: ['前端工程师', '算法工程师', '产品经理']
+        notices: [{
+                img: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+                text: '截止2018年6月12日 数据已经超过650000条'
+            },
+            {
+                img: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+                text: '上线啦'
+            },
+            {
+                img: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+                text: '截止2018年6月12日 数据已经超过650000条'
+            }
+        ],
+        jobArr: ['前端工程师', '算法工程师', '产品经理','前端工程师', '算法工程师', '产品经理','前端工程师', '算法工程师', '产品经理']
     }
 })
