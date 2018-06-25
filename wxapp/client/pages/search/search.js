6720f8d4135df0f3c6cc5f13f8f640d681c01707
duxq <dxq.duxq@lieluobo.com>
2018-06-25 17:30:23 +0800

    finish search view

--- wxapp/client/pages/search/search.js
+++ wxapp/client/pages/search/search.js
@@ -2,10 +2,13 @@
 Page({
     data: {
         loading: false,
+        minLoading: false,
         searchVal: ''
     },
-    search(){
+    search(e){
+        console.log(e.detail)
         this.setData({
+            searchVal: e.detail.value.value,
             loading: true
         })
     }
