dcd78200dfb189b57ac0c45202c8a436fad930a7
duxq <dxq.duxq@lieluobo.com>
2018-06-27 18:14:30 +0800

    add cover

--- wxapp/client/pages/search/search.js
+++ wxapp/client/pages/search/search.js
@@ -3,13 +3,26 @@ Page({
     data: {
         loading: false,
         minLoading: false,
-        searchVal: ''
+        searchVal: '12',
+        isShow: true,
+        searchResults: [1, 2]
     },
     search(e){
         console.log(e.detail)
         this.setData({
             searchVal: e.detail.value.value,
             loading: true
+        })
+    },
+    closeDetail(){
+        this.setData({
+            isShow: true
+        })
+    },
+    showDetail(e){
+        console.log(e.currentTarget.dataset.id)
+        this.setData({
+            isShow: false
         })
     }
 })
