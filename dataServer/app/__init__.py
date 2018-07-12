8efc50f50b175016b1be03f3dcbb9a6db6877521
duxq <dxq.duxq@lieluobo.com>
2018-07-12 15:09:05 +0800

    无关紧要的修改

--- dataServer/app/__init__.py
+++ dataServer/app/__init__.py
@@ -26,7 +26,7 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
     moment.init_app(app)
-    # db.init_app(app)
+    db.init_app(app)
     bootstrap.init_app(app)
     login_manager.init_app(app)
 
