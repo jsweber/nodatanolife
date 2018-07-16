0deab94146f17a66400a66cb73b950ef232709a4
jsweber <951092973@qq.com>
2018-07-16 13:08:31 +0800

    add form

--- dataServer/app/main/views.py
+++ dataServer/app/main/views.py
@@ -1,7 +1,10 @@
 import os, sys
 from datetime import datetime
 from flask_login import login_required
-from flask import Flask, make_response, url_for, request, json, jsonify, session, redirect, render_template
+from flask import Flask, make_response, url_for, request, json, jsonify, session, redirect, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 parentDir = os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 sys.path.insert(0, parentDir)
@@ -10,6 +13,10 @@ from __init__ import db
 from models import Job
 import json
 
+class NameForm(FlaskForm):
+    name = StringField('what is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
 @main.route('/', methods=['GET', 'POST'])
 def index():
     
@@ -17,11 +24,22 @@ def index():
 
 @main.route('/login', methods=['GET', 'POST'])
 def login():
-    print(request.args.get('name', 'none'))
-    if request.method == 'POST':
-        return 'post'
-    elif request.method == 'GET':
-        return jsonify(username=request.args.get('name', 'nobody'), age=30, location='shanghai')
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            print('changed')
+            return flash('name is changed!')
+        session['name'] = form.name.data
+        return redirect(url_for('main.index'))
+    return render_template('login.html', form=form, name=session.get('name'))
+    
+# def login():
+#     print(request.args.get('name', 'none'))
+#     if request.method == 'POST':
+#         return 'post'
+#     elif request.method == 'GET':
+#         return jsonify(username=request.args.get('name', 'nobody'), age=30, location='shanghai')
 
 @main.route('/test', methods=['GET', 'POST'])
 def test():
@@ -44,3 +62,5 @@ def count(name):
     c = db.session.query(Job.salary).filter(Job.job_name.op('regexp')(name)).all()
     resp = make_response('<p>'+ str(c) +'</p>')
     return resp
+
+
