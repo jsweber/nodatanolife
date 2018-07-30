ac30bd6cfcb50272d348f4ba0397fe5201ef5e83
duxq <dxq.duxq@lieluobo.com>
2018-07-30 10:24:05 +0800

    fixed third

--- dataServer/app/__init__.py
+++ dataServer/app/__init__.py
@@ -43,8 +43,8 @@ def create_app(config_name):
     
     @app.route('/')
     def index():
-        return url_for('main.index')
-        
+        return redirect(url_for('main.index'))
+
     @app.errorhandler(404)
     def page_not_found(e):
         return render_template('404.html'), 404
